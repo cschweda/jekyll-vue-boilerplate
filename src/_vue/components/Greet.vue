a79cf6cf64dadd8743440ab1aa6b378a72b5c87a
Chris Schweda <cschweda@gmail.com>
2017-12-15 12:41:02 -0600

    Add props through data attribute

---
+++ src/_vue/components/Greet.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Prop passed via data attribute: <span style="color: #aaa">{{message}}</span></h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Greet',
+  props: ['message'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {color: blue}
+</style>
--- src/_vue/index.js
+++ src/_vue/index.js
@@ -6,12 +6,14 @@ window.$ = window.JQuery = jQuery;
 import Vue from 'vue';
 import Hello from './components/Hello.vue';
 import MyFooter from './components/Footer.vue';
+import Greet from './components/Greet.vue';
 
 import '../_sass/app.scss'
 
 const APPS = {
   Hello,
-  MyFooter
+  MyFooter,
+  Greet
 };
 
 // Vue.config.devtools = true
@@ -20,7 +22,10 @@ function renderAppInElement(el) {
   let App = APPS[el.id];
   if (!App) return;
 
+  // Props as data attributes:
+  // <div class="__vue-root" data-message="Hello" id="Greet"></div>
   const props = Object.assign({}, el.dataset);
+  console.log(props)
   new Vue ({
     el,
     render(createElem) {
